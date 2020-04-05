ede6f5c5aabefe6c09c5d78037ff244ce847b583
Itay Wolfish <itay@linnovate.net>
2020-04-05 10:18:03 +0300

    added video player

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import './ExploreContainer.css';
+import React from "react";
+import VideoPlayer from "../components/VideoPlayer";
+import "./ExploreContainer.css";
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    width: 350,
+    height: 225,
+    sources: [
+      {
+        src: "http://192.168.1.108:8080/live/123.m3u8",
+        type: "application/vnd.apple.mpegurl",
+      },
+    ],
+  };
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        Explore{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+      </p>
+      <VideoPlayer {...videoJsOptions} />
     </div>
   );
 };
