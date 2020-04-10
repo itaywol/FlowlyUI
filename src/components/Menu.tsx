4a27941151845e42108ff2fb399bfb15ea5a5257
Itay Wolfish <itay@linnovate.net>
2020-04-10 10:31:18 +0300

    stream page wip

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -25,6 +25,8 @@ import {
   trashSharp,
   warningOutline,
   warningSharp,
+  compassOutline,
+  cameraOutline,
 } from "ionicons/icons";
 import "./Menu.css";
 
@@ -41,40 +43,10 @@ interface AppPage {
 
 const appPages: AppPage[] = [
   {
-    title: "Inbox",
-    url: "/page/Inbox",
-    iosIcon: mailOutline,
-    mdIcon: mailSharp,
-  },
-  {
-    title: "Outbox",
-    url: "/page/Outbox",
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp,
-  },
-  {
-    title: "Favorites",
-    url: "/page/Favorites",
-    iosIcon: heartOutline,
-    mdIcon: heartSharp,
-  },
-  {
-    title: "Archived",
-    url: "/page/Archived",
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp,
-  },
-  {
-    title: "Trash",
-    url: "/page/Trash",
-    iosIcon: trashOutline,
-    mdIcon: trashSharp,
-  },
-  {
-    title: "Spam",
-    url: "/page/Spam",
-    iosIcon: warningOutline,
-    mdIcon: warningSharp,
+    title: "Explore",
+    url: "/page/stream",
+    iosIcon: compassOutline,
+    mdIcon: compassOutline,
   },
 ];
 
